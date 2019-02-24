0afd42db0ade94011e18ba5281ad892fca3f5cf9
Eric Bragas <ericbragas412@gmail.com>
2019-02-24 14:41:12 -0800

    Finish workflow

--- workflow.sh
+++ workflow.sh
@@ -25,3 +25,24 @@ FRAMEWORK="SCIKIT_LEARN"
 gcloud ml-engine local predict --model-dir=$MODEL_DIR \
     --json-instances $INPUT_FILE \
     --framework $FRAMEWORK
+
+# Create model in ml-engine
+echo "Creating ML-Engine model..."
+gcloud ml-engine models create $MODEL_NAME
+
+VERSION_NAME="v01"
+
+echo "Creating model version..."
+gcloud ml-engine versions create $VERSION_NAME \
+    --model $MODEL_NAME \
+    --origin $MODEL_DIR \
+    --runtime-version=1.12 \
+    --framework $FRAMEWORK \
+    --python-version=3.5
+
+# Testing online prediction
+echo "Testing online prediction..."
+
+gcloud ml-engine predict --model $MODEL_NAME \
+    --version $VERSION_NAME \
+    --json-instances $INPUT_FILE
